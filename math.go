dc250c0b4c5035f211dba9b92da299990e29a01e
Guilherme Ferreira Silva <96479284+guilsferreir@users.noreply.github.com>
2025-11-15 12:14:29 -0300

    feat: :sparkles: add new math methods (#5)
    * feat: :sparkles: add new math methods
    
    adding new math methods like sub, mul and div
    
    * feat: :sparkles: add new math methods
    
    * fix: :bug: import math module
    
    * test: :white_check_mark: increase tests coverage
    
    ---------
    
    Co-authored-by: guilsferreir <guilherme.usferreira@mercadolivre.com>

--- math.go
+++ math.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
 	fmt.Println(sum(1, 2))
@@ -8,4 +9,32 @@ func main() {
 
 func sum(a int, b int) int {
 	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mul(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
+
+func mod(a int, b int) int {
+	return a % b
+}
+
+func sqrt(a int) int {
+	return int(math.Sqrt(float64(a)))
+}
+
+func cbrt(a int) int {
+	return int(math.Cbrt(float64(a)))
+}
+
+func log(a int) int {
+	return int(math.Log(float64(a)))
 }
