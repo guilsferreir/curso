e4343161e076a32f17ed82f34c9b883aa380a767
guilsferreir <guilherme.usferreira@mercadolivre.com>
2025-11-15 12:00:06 -0300

    feat: :sparkles: add new math methods
    adding new math methods like sub, mul and div

--- math.go
+++ math.go
@@ -9,3 +9,15 @@ func main() {
 func sum(a int, b int) int {
 	return a + b
 }
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mul(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
