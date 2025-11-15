5d5e64fb0ff01a9046ff776f51e497efd32017a7
guilsferreir <guilherme.usferreira@mercadolivre.com>
2025-11-15 12:03:00 -0300

    feat: :sparkles: add new math methods

--- math.go
+++ math.go
@@ -21,3 +21,19 @@ func mul(a int, b int) int {
 func div(a int, b int) int {
 	return a / b
 }
+
+func mod(a int, b int) int {
+	return a % b
+}
+
+func sqrt(a int) int {
+	return int(math.Sqrt(float64(a)))
+}
+
+func cbrt(a int) int {
+	return int(math.Cbrt(float64(a)))
+}
+
+func log(a int) int {
+	return int(math.Log(float64(a)))
+}
